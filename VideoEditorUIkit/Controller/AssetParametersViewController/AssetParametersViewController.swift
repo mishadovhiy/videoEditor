9e0f9c0f5c3ac5d24c6da102d2302bce5a4854ac
mishadovhiy <hi@mishadovhiy.com>
2024-01-11 00:18:40 +0200

    tracks: scrollview: fixed when scroll view is not declaring and then while player playing scroll view is not changing

--- VideoEditorUIkit/Controller/AssetParametersViewController/AssetParametersViewController.swift
+++ VideoEditorUIkit/Controller/AssetParametersViewController/AssetParametersViewController.swift
@@ -12,15 +12,17 @@ class AssetParametersViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var assetStackView: UIStackView!
-
+    
     var assetData:MovieGeneralParameterList = .test
     static var rowsHeight:CGFloat = 20
     static let durationWidthMultiplier:CGFloat = 15
+    var scrollViewDeclaring = false
+    
     var tableData:[MovieGeneralParameterList.AssetsData] {
         return assetData.asstes
     }
     private var ignoreScroll:Bool = false
-
+    
     var parentVC: EditorViewController? {
         return parent as? EditorViewController
     }
@@ -29,37 +31,49 @@ class AssetParametersViewController: UIViewController {
         super.viewDidLoad()
         loadUI()
     }
-
+    
     func scrollPercent(_ percent:CGFloat) {
         if !ignoreScroll {
             scrollView.contentOffset.x = (scrollView.contentSize.width - self.view.frame.width) * percent
+            
+        }
+    }
+}
 
+
+extension AssetParametersViewController {
+    func scrollingEnded() {
+        scrollViewDeclaring = false
+        ignoreScroll = false
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        ignoreScroll = true
+    }
+    
+    func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
+        scrollViewDeclaring = true
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if !scrollViewDeclaring {
+            scrollingEnded()
         }
     }
     
-    
-
-    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        print(#function)
-        ignoreScroll = true
-    }
-
-    
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        print(#function)
-
-        ignoreScroll = false
-      //  parentVC?.playerVC?.play()
+        scrollingEnded()
     }
     
-     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-         if !ignoreScroll {
-             return
-         }
-         let percent = scrollView.contentOffset.x / (scrollView.contentSize.width - self.view.frame.width)
-         parentVC?.seek(percent: percent)
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if !ignoreScroll {
+            return
+        }
+        let percent = scrollView.contentOffset.x / (scrollView.contentSize.width - view.frame.width)
+        parentVC?.seek(percent: percent)
     }
 }
+
 
 extension AssetParametersViewController:AssetAttachmentViewDelegate {
     func attachmentSelected(_ data: MovieAttachmentProtocol?) {
