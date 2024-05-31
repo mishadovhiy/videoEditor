aa9f2b5e4764e78bd43204fe1db950b9368cd64e
mishadovhiy <hi@mishadovhiy.com>
2024-05-31 17:16:31 +0300

    tutorialvc: higlight selection

--- VideoEditorUIkit/Mics/Extension/UIView.swift
+++ VideoEditorUIkit/Mics/Extension/UIView.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 extension UIView {
+    func convertedToImage(bounds:CGRect? = nil) -> UIImage? {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds ?? self.bounds)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
+    }
+    
     var viewController:UIViewController? {
         var nextResponder: UIResponder? = self
         var attempt = 2000
